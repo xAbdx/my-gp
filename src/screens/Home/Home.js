8a2660dc64577dfa80a679dff400c44b4eef35ee
xAbdx <67425264+xAbdx@users.noreply.github.com>
2023-12-04 16:44:58 +0200

    must visit section added

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -25,6 +25,7 @@ const Home = () => {
     return (
         <div>
             <ResponsiveAppBar />
+            {/* Hero Section */}
             <div class="containerHero">
                 <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
                     <div class="smaller">
@@ -135,11 +136,15 @@ const Home = () => {
                         </div>
                     </div>
                 </Box>
+                {/* Cards Section */}
                 <Box sx={{ display: { xs: 'flex', justifyContent: 'center' } }}>
                     <div class="cardSection">
                         <Card sx={{ width: '250px', bgcolor: '#F8F8F8', margin: '40px 40px 40px 0', borderRadius: '10%', boxShadow: 0 }}>
                             <CardContent>
                                 <Typography variant="h3" component="div" sx={{
+                                    display: 'flex',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
                                     fontWeight: 'bold',
                                 }}>
                                     <TravelExploreIcon fontSize="larger" sx={{ color: '#463DCF' }} />
@@ -183,6 +188,9 @@ const Home = () => {
                         <Card sx={{ width: '250px', bgcolor: '#F8F8F8', margin: '40px 40px 40px 0', borderRadius: '10%', boxShadow: 0 }}>
                             <CardContent>
                                 <Typography variant="h3" component="div" sx={{
+                                    display: 'flex',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
                                     fontWeight: 'bold',
                                 }}>
                                     <TravelExploreIcon fontSize="larger" sx={{ color: '#463DCF' }} />
@@ -226,6 +234,9 @@ const Home = () => {
                         <Card sx={{ width: '250px', bgcolor: '#F8F8F8', margin: '40px 40px 40px 0', borderRadius: '10%', boxShadow: 0 }}>
                             <CardContent>
                                 <Typography variant="h3" component="div" sx={{
+                                    display: 'flex',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
                                     fontWeight: 'bold',
                                 }}>
                                     <TravelExploreIcon fontSize="larger" sx={{ color: '#463DCF' }} />
@@ -269,6 +280,7 @@ const Home = () => {
                     </div>
                 </Box>
             </div>
+            {/* Must-Visit Section */}
             <Box sx={{
                 width: '100%',
                 minHeight: '700px',
@@ -278,6 +290,7 @@ const Home = () => {
                 paddingTop: '50px',
                 flexDirection: 'column',
                 alignItems: 'center',
+                paddingBottom: '50px'
             }}>
                 <p class="titleSThree">Must-visit attractions</p>
                 <div class="mapSection">
@@ -313,7 +326,72 @@ const Home = () => {
                     backgroundColor: '#777777',
                     height: 1.4,
                     width: '60%',
+                    marginTop: '40px'
                 }} />
+                <div class="mapSection">
+                    <div>
+                        <iframe
+                            class="map"
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3400.9828599193343!2d35.10678373802149!3d31.52463070444061!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1502e70bd58ecbd5%3A0x4ea883b874c58383!2sHebron%20Tourism!5e0!3m2!1sen!2s!4v1701699587789!5m2!1sen!2s"
+                            width="400"
+                            height="250"
+                            style={{ border: "0", borderRadius: '3%' }}
+                            allowfullscreen=""
+                            loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade"
+                        ></iframe>
+                    </div>
+                    <div class="mapContent">
+                        <h5>Old city</h5>
+                        <p>it's a historically rich area known for its ancient architecture, bustling markets, and religious significance. It houses the Ibrahimi Mosque, a revered site for Muslims, and boasts narrow, winding streets filled with shops, eateries, and cultural heritage.</p>
+                        <div class="contentWraper">
+                            <div class="insideContentWraper">
+                                <h6>Price</h6>
+                                <p><AttachMoneyIcon sx={{ color: '#267E02' }} />free entry</p>
+                            </div>
+                            <div>
+                                <h6>Time</h6>
+                                <p><AccessTimeIcon sx={{ color: '#F96719' }} /> 3h</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr style={{
+                    color: '#777777',
+                    backgroundColor: '#777777',
+                    height: 1.4,
+                    width: '60%',
+                    marginTop: '40px'
+                }} />
+                <div class="mapSection">
+                    <div>
+                        <iframe
+                            class="map"
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1700.261093249257!2d35.08331838985911!3d31.537280831870877!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1502e68032b08b7d%3A0x9036dbe92b030019!2zVGhlIENhdGhlZHJhbCBvZiB0aGUgSG9seSBUcmluaXR5IC0g2YPYp9iq2K_Ysdin2KbZitipINin2YTYq9in2YTZiNirINin2YTYo9mC2K_Ysw!5e0!3m2!1sen!2s!4v1701699749907!5m2!1sen!2s"
+                            width="400"
+                            height="250"
+                            style={{ border: "0", borderRadius: '3%' }}
+                            allowfullscreen=""
+                            loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade"
+                        ></iframe>
+                    </div>
+                    <div class="mapContent">
+                        <h5>The Cathedral of the
+                            Holy Trinity</h5>
+                        <p>The Church of the Holy Forefathers and Monastery of the Holy Trinity also known as Al Maskobiya, is a Russian Orthodox monastery and church in Hebron, Palestine, founded in the 20th century on the site of the ancient Oak of Mamre.</p>
+                        <div class="contentWraper">
+                            <div class="insideContentWraper">
+                                <h6>Price</h6>
+                                <p><AttachMoneyIcon sx={{ color: '#267E02' }} />free entry</p>
+                            </div>
+                            <div>
+                                <h6>Time</h6>
+                                <p><AccessTimeIcon sx={{ color: '#F96719' }} /> 30m</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </Box>
             {/* <Footer /> */}
 
