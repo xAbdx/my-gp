6bd9a8e64a3245f5322dfc3929e802f421045934
xAbdx <67425264+xAbdx@users.noreply.github.com>
2024-01-04 17:04:20 +0200

    fixing some issues in askme file

--- src/screens/AskMe/Askme.jsx
+++ src/screens/AskMe/Askme.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
+import './Askme.css';
 import AskmeNav from '../../components/AskmeNav/AskmeNav.js';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
@@ -6,10 +7,11 @@ import Gptlogo from '../../components/Gptlogo.tsx';
 import IconButton from '@mui/material/IconButton';
 import SendIcon from '@mui/icons-material/Send';
 import InputAdornment from '@mui/material/InputAdornment';
-import './Askme.css';
 import Avatar from '@mui/material/Avatar';
 import AppBar from '@mui/material/AppBar';
 import Paper from '@mui/material/Paper';
+import CircularProgress from '@mui/material/CircularProgress';
+import Tooltip from '@mui/material/Tooltip';
 
 const API_KEY = process.env.REACT_APP_MY_OPENAI_KEY;
 
@@ -17,10 +19,23 @@ export default function Home() {
 
     const [question, setQuestion] = useState('');
     const [response, setResponse] = useState([]);
+    const [showProgress, setShowProgress] = useState(false);
     const ref = useRef();
 
+    const handleEnterKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            callOpenAIAPI();
+        }
+    };
+
     async function callOpenAIAPI() {
+        if (question.trim() === '') {
+            return; // Do nothing if the question is empty or contains only whitespace
+        }
+
         // console.log("calling the api");
+        setShowProgress(true); // Show CircularProgress
 
         const APIBody = {
             model: 'gpt-3.5-turbo',
@@ -48,22 +63,21 @@ export default function Home() {
         }).then((data) => {
             return data.json();
         }).then((data) => {
-            console.log(data);
-
+            // console.log(data);
             const apiResponse = data.choices[0].message.content;
-            // setResponse(data.choices[0].message.content);
             setResponse([
                 ...response,
                 { text: text, isUser: true },
                 { text: apiResponse, isUser: false },
             ])
         });
+        setQuestion('');
+        setShowProgress(false);
     }
-
     // console.log(question);
 
     useEffect(() => {
-        setTimeout(() => ref.current.scrollIntoView({ behavior: "smooth" }), 500)
+        setTimeout(() => ref.current.scrollIntoView({ behavior: "smooth" }), 300)
     }, [response])
 
 
@@ -71,7 +85,7 @@ export default function Home() {
         <div class="askMeContainer">
             <AskmeNav />
             <React.Fragment>
-                <Paper sx={{ pb: '50px', bgcolor: "#393b46" }}>
+                <Paper elevation={0} sx={{ pb: '50px', bgcolor: "#393b46" }}>
                     <div class="askMeDiv">
                         <div class="askMe">
                             {response.map((m, index) => (
@@ -88,9 +102,9 @@ export default function Home() {
                             ))}
                             <div ref={ref}></div>
                         </div>
+                        {showProgress && <CircularProgress />} {/* Show CircularProgress conditionally */}
                     </div>
                 </Paper>
-
                 <AppBar position="fixed"
                     sx={{
                         top: 'auto',
@@ -103,22 +117,31 @@ export default function Home() {
                     <Box
                         sx={{
                             width: 500,
-                            maxWidth: '100%',
+                            maxWidth: '90%',
                             bgcolor: "#555666",
                             margin: '20px 0',
+                            borderRadius: "14px",
                         }}
                     >
                         <TextField
                             value={question}
                             onChange={(e) => setQuestion(e.target.value)}
+                            onKeyDown={handleEnterKeyPress} // Call the function on key down
                             placeholder='Ask me'
-                            sx={{ width: '100%', borderRadius: "4%", color: "#D9D9E3" }}
+                            sx={{
+                                width: '100%',
+                                borderRadius: "14px",
+                                input: { color: '#D9D9E3', },
+                            }}
                             InputProps={{
-                                endAdornment: <InputAdornment position="end">
-                                    <IconButton sx={{ color: "#e0e0ec" }}>
-                                        <SendIcon onClick={callOpenAIAPI} />
-                                    </IconButton>
-                                </InputAdornment>
+                                endAdornment:
+                                    <InputAdornment position="end">
+                                        <Tooltip title="Send message" placement="top" arrow>
+                                            <IconButton sx={{ color: "#e0e0ec" }}>
+                                                <SendIcon onClick={callOpenAIAPI} />
+                                            </IconButton>
+                                        </Tooltip>
+                                    </InputAdornment>
                             }}
                         />
                     </Box>
